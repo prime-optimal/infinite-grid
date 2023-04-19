b0c860be3d829b49c6fb47ae2819a5ccbe92a2cb
optimal prime <90554087+prime-optimal@users.noreply.github.com>
2023-04-18 18:20:30 -0700

    adjusted startserver.sh to kill running gunicorn and caddy processes

--- site/startserver.sh
+++ site/startserver.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+# Stop any running Gunicorn and Caddy processes
+pkill gunicorn
+pkill caddy
+
 if [ "$DEPLOY_ENV" = "production" ]; then
     cp Caddyfile.production Caddyfile
 else
     cp Caddyfile.local Caddyfile
 fi
-
 
 # Start Gunicorn server
 gunicorn --bind 0.0.0.0:8080 --workers=4 app:app &
